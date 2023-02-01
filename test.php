96d2f88937a1f266a1556b70531880063d2737c2
nilay <nilayanand@gmail.com>
2023-02-01 12:59:29 +0530

    make index page as php

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include('common_head.inc.php') ?>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php include('common_header.inc.php') ?>
+  
+  <!-- ======= Footer ======= -->
+  <?php include('common_footer.inc.php')?>
+
+</body>
+
+</html>
